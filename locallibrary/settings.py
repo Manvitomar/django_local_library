fa3fec2e429e0a4b76fdfa5c88e76d19931c5ed3
Manvitomar <manvitomar046@gmail.com>
2026-05-23 16:33:31 +0530

    working_on_models

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'catalog.apps.CatalogConfig',
+    'catalog',
 ]
 
 MIDDLEWARE = [
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'locallibrary',
+        'USER': 'root',
+        'PASSWORD': 'manu99',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
